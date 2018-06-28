969b441131488a3f0720d69e1ff8420a18f42f27
Romashka <qwertyromchik@mail.ru>
2018-06-28 19:14:17 +0300

    added cart controller

--- war/src/main/java/com/eshop/products/controllers/CartController.java
+++ war/src/main/java/com/eshop/products/controllers/CartController.java
@@ -1,8 +1,32 @@
 package com.eshop.products.controllers;
 
+import com.eshop.products.entities.Product;
+import com.eshop.products.services.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class CartController {
 
+    @Autowired
+    private CartService cartService;
+
+    @RequestMapping("/cart")
+    public ModelAndView showAllProductInCart(int cartID) {
+        List<Product> productList = cartService.showAllProductInCart(cartID);
+        return new ModelAndView("cart", "list", productList);
+    }
+
+    @RequestMapping("/deleteFromCart")
+    public ModelAndView deleteProductFromCart(int cartID,int productID) {
+        cartService.removeProductFromCart(productID,cartID);
+        List<Product> productList = cartService.showAllProductInCart(cartID);
+        return new ModelAndView("cart", "list", productList);
+    }
+
+    //TODO adding products
 }
